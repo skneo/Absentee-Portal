cbea58548b0bf4e34c1e44c481e4a70d36bb113b
Satish Kushwah <satishkushwahdigital@gmail.com>
2022-09-18 17:27:16 +0530

    section name in upper case in title

--- fill_leaves.php
+++ fill_leaves.php
@@ -14,7 +14,7 @@ $section = $_GET['section'];
     <!-- Bootstrap CSS -->
     <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3' crossorigin='anonymous'>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <title>Fill Leave Statements - <?php echo ucfirst($section) ?></title>
+    <title>Fill Leave Statements - <?php echo strtoupper($section) ?></title>
 </head>
 
 <body>
